c9d8afb5a38a2e3cf4131de2f9ad0f40b45b2df2
objectmikael <mikael.borneo@gmail.com>
2023-07-22 16:13:53 -0400

    removed services

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Services from './Services';
+// import Services from './Services';
 import AboutUs from './AboutUs';
 import Contact from './Contact';
 // import Home from './Home';
@@ -11,7 +11,7 @@ export default function Main() {
     <div className="main">
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/services" element={<Services />} />
+        {/* <Route path="/services" element={<Services />} /> */}
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/get-a-quote" element={<GetAQuote />} />
