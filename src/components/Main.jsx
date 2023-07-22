96c65e46f1287d1c74dae34c042bb1bcd868df14
objectmikael <mikael.borneo@gmail.com>
2023-07-22 16:12:17 -0400

    insert services, aboutus, contact, getaquote

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-// import Services from './Services';
-// import AboutUs from './AboutUs';
-// import Contact from './Contact';
-import Home from './Home';
-// import GetAQuote from './GetAQuote';
+import Services from './Services';
+import AboutUs from './AboutUs';
+import Contact from './Contact';
+// import Home from './Home';
+import GetAQuote from './GetAQuote';
 
 export default function Main() {
   return (
     <div className="main">
       <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/services" element={<Services />} />
+        {/* <Route path="/" element={<Home />} /> */}
+        <Route path="/services" element={<Services />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/get-a-quote" element={<GetAQuote />} /> */}
+        <Route path="/get-a-quote" element={<GetAQuote />} />
       </Routes>
     </div>
   );
