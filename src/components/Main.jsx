6175a698ea939472c00cee5b39f4e8e9758a74ad
objectmikael <mikael.borneo@gmail.com>
2023-07-22 16:09:35 -0400

    insert home component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,22 +1,21 @@
 import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 // import Services from './Services';
 // import AboutUs from './AboutUs';
 // import Contact from './Contact';
-// import Home from './Home';
+import Home from './Home';
 // import GetAQuote from './GetAQuote';
 
 export default function Main() {
   return (
     <div className="main">
-      <h1>Main Test</h1>
-      {/* <Routes>
+      <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
+        {/* <Route path="/services" element={<Services />} />
         <Route path="/about-us" element={<AboutUs />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/get-a-quote" element={<GetAQuote />} />
-      </Routes> */}
+        <Route path="/get-a-quote" element={<GetAQuote />} /> */}
+      </Routes>
     </div>
   );
 }
